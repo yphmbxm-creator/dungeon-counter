799ee87ec2c76870493d2f3481ccae5ec651f98a
yphmbxm-creator <yphmbxm@gmail.com>
2026-02-04 22:47:46 +0800

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,21 @@
+﻿const CACHE_NAME = "dungeon-counter-v1";
+const FILES_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icon.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(
+      response => response || fetch(event.request)
+    )
+  );
+});
